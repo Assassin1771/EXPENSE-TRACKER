3fffb72c79835d84c6f6f122f036722674234d31
Tushar Garg <tshrgarg2010@gmail.com>
2022-01-20 14:43:06 +0530

    Resolved few UI/UX bugs and itegrated Speechly

--- app/src/index.js
+++ app/src/index.js
@@ -1,15 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { SpeechProvider } from '@speechly/react-client';
 
-import { Provider } from "./context/Context";
-import App from "./app";
+import { Provider } from './context/context';
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
-  <>
+  <SpeechProvider appId="7c4aee08-1073-4a32-b862-ebe1850e0732" language="en-US">
     <Provider>
       <App />
     </Provider>
-  </>,
-  document.getElementById("root")
+  </SpeechProvider>,
+  document.getElementById('root'),
 );
