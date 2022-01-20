eccbc0d5ae94f7bef701830080b7a62783ead35e
Tushar Garg <tshrgarg2010@gmail.com>
2022-01-20 21:13:59 +0530

    Logo change and CSS Bug Fixes

--- app/src/components/InfoCard.js
+++ app/src/components/InfoCard.js
@@ -7,9 +7,9 @@ const InfoCard = () => {
     <div elevation={3} style={{ textAlign: "center", padding: "0 10%" }}>
       Try saying: <br />
       Add {isIncome ? "Income " : "Expense "}
-      for {isIncome ? "₹100 " : "₹50 "}
+      for {isIncome ? "₹1000 " : "₹500 "}
       in Category {isIncome ? "Salary " : "Travel "}
-      for {isIncome ? "Monday " : "Thursday "}
+      for {isIncome ? "Monday " : "Sunday "}
     </div>
   );
 };
